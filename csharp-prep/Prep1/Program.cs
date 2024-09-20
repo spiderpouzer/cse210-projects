955186859f5c9a8059cc9a1e2b23fea11ce2bc6f
spiderpouzer <168085741+spiderpouzer@users.noreply.github.com>
2024-09-20 17:59:18 -0300

    Add files via upload

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Whats your first name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Whats your last name?");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"My name is {lastName}, {firstName} {lastName}");
     }
 }
